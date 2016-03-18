3ca9d4b143da073f1978bc87bcbf2bef05095ae1
Gilles Danjou <gilles.danjou@gmail.com>
2016-03-18 12:30:24 +0100

    first commit

---
+++ mobile/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-device": "1.1.1",
+    "cordova-plugin-splashscreen": "3.2.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ servers.js
@@ -0,0 +1,2 @@
+var wakandaServerURL = 'http://localhost:8082/';
+var nodeServerURL = 'http://localhost:8200';
