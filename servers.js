6b8b3e916883ed7d2d028ea8dc620a5ce4e1bb34
Gilles Danjou <gilles.danjou@gmail.com>
2016-03-18 19:12:30 +0100

    Update servers.js

--- servers.js
+++ servers.js
@@ -1,2 +1,2 @@
-wakandaServerURL = 'http://733abaee.ngrok.io';
-nodeServerURL = 'http://099f5f1b.ngrok.io';
+wakandaServerURL = 'http://733abaee.ngrok.io/';
+nodeServerURL = 'http://099f5f1b.ngrok.io/';
